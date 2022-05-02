0c16c5240d73f809e7cbd984027f1cc345598353
MMMike <mikem.tba@gmail.com>
2022-05-02 14:14:39 +0200

    feat: :bug: Vercel Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-static'
+// import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 import { mdsvex } from 'mdsvex'
 import { resolve } from 'path'
